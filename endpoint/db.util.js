c0841ff2e074af0bba5799196144673ebeef2159
Utkarsh Tyagi <Utkarsh.Tyagi@mea.ey.net>
2018-03-18 23:41:45 +0530

    labs url

--- endpoint/db.util.js
+++ endpoint/db.util.js
@@ -5,7 +5,7 @@ var db;
 
 let connectToMongoDB = () => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(mongoUrlLocal, function (err, dbInstance) {
+    MongoClient.connect(mongoUrlLabs, function (err, dbInstance) {
       if (err) return reject({ Error: "MongoDB" });
       db = dbInstance;
       return resolve();
